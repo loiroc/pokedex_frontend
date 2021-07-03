e1660af5623a3cc971ee6290fee8ea63ced5c131
Lucas Coimbra <loiroc@hotmail.com>
2021-07-02 21:41:21 -0300

    add search filtering pokemon

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,25 +2,30 @@ import React from "react";
 import "../styles/Card.css";
 import "../styles/Types.css";
 
-function Card({id, name, image, types}) {
-
-    function upperFirstChar(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1)
-    }
+function Card({ id, name, image, types }) {
     
-    function mask(string) {
-        if(string.toString().length === 1) return `00${string}`
-        if(string.toString().length === 2) return `0${string}`
-        if(string.toString().length === 3) return `${string}`
-    }
+  function upperFirstChar(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  function mask(string) {
+    if (string.toString().length === 1) return `00${string}`;
+    if (string.toString().length === 2) return `0${string}`;
+    if (string.toString().length === 3) return `${string}`;
+  }
+
   return (
     <div className="Card" key={id}>
-        <img id="pokemon" alt={`${name}`} src={image} />
-        <div className={`card bg-${types[0]}`}>
-            <h4>#{mask(id)}</h4>
-            <h1>{upperFirstChar(name)}</h1>
-            <div id="types">{types.map((type) => { return <p className={`p-${type}`}>{upperFirstChar(type)}</p> })}</div>
-            </div>
+      <img id="pokemon" alt={`${name}`} src={image} />
+      <div className={`card bg-${types[0]}`}>
+        <h4>#{mask(id)}</h4>
+        <h1>{upperFirstChar(name)}</h1>
+        <div id="types">
+          {types.map((type) => {
+            return <p className={`p-${type}`}>{upperFirstChar(type)}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,71 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "../styles/Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Pokeball from "../components/Pokeball";
 import Card from "../components/Card";
+import Form from "react-bootstrap/Form";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Home() {
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => { fetchData() }, [])
-    
-    const [pokemonData, setPokemonData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchData, setSearchData] = useState([]);
 
-    function fetchData() {
-        axios.get("http://192.168.99.106:3002/pokemon").then((results) => { setPokemonData(results.data) })
-    }
+  function fetchData() {
+    axios.get(`http://192.168.99.106:3002/pokemon`).then((results) => {
+      setPokemonData(results.data);
+      setSearchData(results.data);
+      setLoading(false);
+    });
+  }
 
   return (
     <div className="Home">
-        <div className="container">
-        {pokemonData.length > 0 ? pokemonData.map((pokemon) => { return <Card id={pokemon.id} name={pokemon.name} image={pokemon.image} types={pokemon.types} /> }) : <Pokeball/>} 
+      <div className="logo">
+        <img alt="Pokemon" src="images/logo.png" />
+      </div>
+      {!loading && (
+        <div className="top">
+          <SearchIcon className="SearchIcon" />
+          <Form.Control
+            style={{ width: "50%" }}
+            type="text"
+            placeholder="Pesquise por um Pokemon"
+            onChange={(e) => {
+              setSearchData(
+                pokemonData.filter((pokemon) => {
+                  return pokemon.name.includes(e.target.value);
+                })
+              );
+            }}
+          />
         </div>
+      )}
+      {loading ? (
+        <Pokeball />
+      ) : searchData.length > 0 ? (
+        <div className="container">
+          {searchData.map((pokemon) => {
+            return (
+              <Card
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.image}
+                types={pokemon.types}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div id="center">
+          <h2>Nenhum resultado encontrado</h2>
+        </div>
+      )}
     </div>
   );
 }
